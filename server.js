1e3b7c0dad895a1bc504b3d0529c472fb5801ef5
mcmx385 <50856973+mcmx385@users.noreply.github.com>
2021-07-15 11:19:13 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express =require('express')
+const express = require('express')
 const path = require('path')
 
-const app =express()
+const app = express()
 
 app.use(express.static('./dist/ngx-temp'))
 
-app.get('/*',(req,res)=>{
-    res.sendfile('index.html', {root: 'dist/ngx-temp'})
+app.get('/*', (req, res) => {
+    res.sendFile('index.html', { root: 'dist/ngx-temp' })
 })
 
-app.listen(process.env.PORT || 4200,()=>{
+app.listen(process.env.PORT || 4200, () => {
     console.log('listening to port')
 })
