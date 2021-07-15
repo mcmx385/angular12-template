8bf353810b5311befccc27db58450908f51ea600
mcmx385 <50856973+mcmx385@users.noreply.github.com>
2021-07-15 11:01:25 +0800

    expressjs

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const app =express()
 app.use(express.static('./dist/ngx-temp'))
 
 app.get('/*',(req,res)=>{
-    res.sendfile('index.html',(root:'dist/ngx-temp'))
+    res.sendfile('index.html', {root: 'dist/ngx-temp'})
 })
 
-app.listen(process.env.PORT || 8000)
+app.listen(process.env.PORT || 4200,()=>{
+    console.log('listening to port')
+})
