34d79132b13613ef7e9eca4d4f9765b0789aa599
mcmx385 <50856973+mcmx385@users.noreply.github.com>
2021-08-06 10:47:02 +0800

    layout

--- src/app/components/pages/user/user.module.ts
+++ src/app/components/pages/user/user.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { UserHomeComponent } from './user-home/user-home.component';
 import { UserComponent } from './user/user.component';
+import { LayoutModule } from '../../layout/layout.module';
 
 @NgModule({
   declarations: [UserComponent, UserHomeComponent],
-  imports: [CommonModule, UserRoutingModule],
+  imports: [CommonModule, UserRoutingModule, LayoutModule],
   exports: [UserComponent, UserHomeComponent],
 })
 export class UserModule {}
